320bd6db6084c14c0e6af6add0f4856a352b7150
sbzhechev <sbzhechev@gmail.com>
2024-08-06 23:36:50 +0300

    Added mongo db requirements.

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,9 +1,60 @@
 const SOCKET_IO_TOKEN = 'SOCKET_IO';
 const CONNECTION_TOKEN = 'CONNECTION';
 const RAID_MODEL_TOKEN = 'RAID_MODEL';
+const PLAYER_MODEL_TOKEN = 'PLAYER_MODEL';
+const CLAN_MODEL_TOKEN = 'CLAN_MODEL';
+
+const PLAYER_DATA_PROPERTIES = [
+  'max_stage', 
+  'player_code', 
+  'country_code', 
+  'crafting_shards_spent', 
+  'raid_wildcard_count', 
+  'current_card_currency', 
+  'additive_relic_multiplier', 
+  'relics_received', 
+  'relics_spent', 
+  'name', 
+  'total_tournaments', 
+  'undisputed_count', 
+  'titan_points', 
+  'total_raid_player_xp', 
+  'player_raid_level', 
+  'total_card_level', 
+  'equipment_set_count', 
+  'total_pet_levels', 
+  'total_skill_points', 
+  'total_helper_weapons', 
+  'total_clan_scrolls', 
+  'challenge_tournaments_participation', 
+  'challenge_tournaments_undisputed_count', 
+  'current_world_id', 
+  'clan_code', 
+  'clan_name', 
+  'role', 
+  'weekly_ticket_count', 
+  'loyalty_level', 
+  'daily_raid_tickets', 
+  'previous_rank', 
+  'artifacts', 
+  'seasonal_relics_received', 
+  'seasonal_relics_spent', 
+  'seasonal_relic_multiplier', 
+  'seasonal_artifacts', 
+  'cards', 
+  'pets', 
+  'badge_count', 
+  'hero_weapon', 
+  'clan_scroll', 
+  'skill_tree', 
+  'equipment_set'
+]
 
 export {
   CONNECTION_TOKEN,
   RAID_MODEL_TOKEN,
-  SOCKET_IO_TOKEN
+  SOCKET_IO_TOKEN,
+  PLAYER_MODEL_TOKEN,
+  PLAYER_DATA_PROPERTIES,
+  CLAN_MODEL_TOKEN
 }
