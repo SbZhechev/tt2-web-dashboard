2196670a6996d22d32b75773129ac7718284220f
sbzhechev <sbzhechev@gmail.com>
2024-08-06 23:36:50 +0300

    Added all new providers to the app module.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { SocketIoService } from './socket-io/socket-io.service';
 import { ConfigModule } from '@nestjs/config';
 import { socketIoProvider } from './socket-io/socket-io.provider';
+import { databaseConnectionProvider } from './mongoDB/databaseConnection.provider';
+import { modelProviders } from './mongoDB/model.providers';
+import { TT2Service } from './tt2.service';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService, SocketIoService, socketIoProvider],
+  providers: [
+    AppService, 
+    SocketIoService, 
+    TT2Service,
+    socketIoProvider, 
+    databaseConnectionProvider,
+    ...modelProviders
+  ],
   imports: [ConfigModule.forRoot({ isGlobal: true})]
 })
 export class AppModule {}
