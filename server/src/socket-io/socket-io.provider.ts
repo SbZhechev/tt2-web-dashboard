2fee4eaa61ce6bb48ea826d1b6b30da2e3232c36
sbzhechev <sbzhechev@gmail.com>
2024-11-12 00:37:41 +0200

    Updated socket service setup logic.

--- server/src/socket-io/socket-io.provider.ts
+++ server/src/socket-io/socket-io.provider.ts
@@ -1,15 +1,20 @@
 import { SOCKET_IO_TOKEN } from "src/constants";
 import { SocketIoService } from "./socket-io.service";
+import { TT2Service } from "src/tt2.service";
 
 export const socketIoProvider = {
   provide: SOCKET_IO_TOKEN,
-  useFactory: (socketIoService: SocketIoService) => {
+  useFactory: (socketIoService: SocketIoService, tt2Service: TT2Service) => {
     let raidInstance = socketIoService.createSocketInstance('raid');
     raidInstance.on('connect', socketIoService.onConnectEventHandler);
     raidInstance.on('attack', socketIoService.onAttackEventHandler);
     raidInstance.on('start', socketIoService.onRaidStartEventHandler);
 
     raidInstance.connect();
+
+    let playerInstance = socketIoService.createSocketInstance('player');
+    // playerInstance.on('connect', tt2Service.getPlayerData);
+    playerInstance.connect();
   },
-  inject: [SocketIoService]
+  inject: [SocketIoService, TT2Service]
 }
